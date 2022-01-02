a56c7692e6283276c200f30e9768c470c6e4b0b7
Shuai Wang <38368612+bearswang@users.noreply.github.com>
2022-01-02 23:31:09 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #!/user/bin/env python3
+
 import os
 import subprocess as sp
 
-sp.run(['bash', '-c', 'python2 jemdoc.py index.jemdoc'])
-sp.run(['bash', '-c', 'python2 jemdoc.py research.jemdoc'])
-sp.run(['bash', '-c', 'python2 jemdoc.py software.jemdoc'])
-sp.run(['bash', '-c', 'python2 jemdoc.py publications.jemdoc'])
-sp.run(['bash', '-c', 'python2 jemdoc.py teaching.jemdoc'])
+sp.run(['python jemdoc.py index.jemdoc'], shell=True)
+sp.run(['python jemdoc.py research.jemdoc'], shell=True)
+sp.run(['python jemdoc.py software.jemdoc'], shell=True)
+sp.run(['python jemdoc.py publications.jemdoc'], shell=True)
+sp.run(['python jemdoc.py teaching.jemdoc'], shell=True)
+sp.run(['python jemdoc.py demo.jemdoc'], shell=True)
